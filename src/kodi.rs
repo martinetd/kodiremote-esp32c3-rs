aa0c869706bcb9c84e869a825d70ac82e146046a
Dominique Martinet <asmadeus@codewreck.org>
2023-11-29 21:07:36 +0900

    kodi: synthetize requests instead of hardcoding strings
    this is a little bit cleaner, and we can apparently afford it

--- src/kodi.rs
+++ src/kodi.rs
@@ -5,18 +5,32 @@ use embedded_svc::{
 };
 use esp_idf_svc::http::client::{Configuration, EspHttpConnection};
 use log::info;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::CONFIG;
 
-// We don't use this as we build json directly
-//#[derive(Seralize)]
-//struct JsonRequest<Payload> {
-//    Jsonrpc: String,
-//    Id: u8,
-//    Method: String,
-//    Params: Payload,
-//}
+#[derive(Serialize)]
+struct JsonRequest<'a, Payload> {
+    jsonrpc: &'a str,
+    id: u8,
+    method: &'a str,
+    params: &'a Payload,
+}
+
+#[derive(Serialize)]
+struct PlayerId {
+    playerid: i8,
+}
+
+#[derive(Serialize)]
+struct Volume {
+    volume: i8,
+}
+
+#[derive(Serialize)]
+struct Properties<'a> {
+    properties: &'a [&'a str],
+}
 
 #[derive(Deserialize)]
 struct JsonResponse<Payload> {
@@ -48,13 +62,22 @@ struct Vol {
     volume: i8,
 }
 
-fn req<Resp>(payload: impl AsRef<[u8]>, get_response: bool) -> Result<Option<Resp>>
+fn req<'a, Req, Resp>(method: &'a str, params: &'a Req, get_response: bool) -> Result<Option<Resp>>
 where
-    Resp: for<'a> Deserialize<'a>,
+    Req: Serialize,
+    Resp: for<'b> Deserialize<'b>,
 {
     if cfg!(feature = "nowifi") {
         return Ok(None);
     }
+
+    let payload = serde_json::to_string(&JsonRequest::<Req> {
+        jsonrpc: "2.0",
+        id: 0,
+        method,
+        params,
+    })?;
+
     let connection = EspHttpConnection::new(&Configuration::default())?;
     let mut client = http::client::Client::wrap(connection);
     let headers = [
@@ -63,7 +86,7 @@ where
     ];
     let mut request = client.post(CONFIG.kodi_endpoint, &headers)?;
     log::info!("Submitting {}", String::from_utf8_lossy(payload.as_ref()));
-    request.write_all(payload.as_ref())?;
+    request.write_all(payload.as_bytes())?;
     request.flush()?;
     let mut response = request.submit()?;
     let status = response.status();
@@ -76,7 +99,7 @@ where
     log::info!("Got {}", String::from_utf8_lossy(&buf[0..size]));
 
     if let Ok(json_error) = serde_json::from_slice::<JsonError>(&buf[0..size]) {
-        return Err(anyhow!("Failed: {}", json_error.error.message))
+        return Err(anyhow!("Failed: {}", json_error.error.message));
     }
     if !get_response {
         return Ok(None);
@@ -87,19 +110,23 @@ where
 }
 
 pub fn play_pause() -> Result<()> {
-    req::<()>(br#"{"jsonrpc": "2.0", "id": 0, "method": "Player.PlayPause", "params": { "playerid": 0 }}"#, false)?;
+    req::<PlayerId, ()>("Player.PlayPause", &PlayerId { playerid: 0 }, false)?;
     Ok(())
 }
 
-pub fn set_vol(vol: i8) -> Result<()> {
-    let payload = format!(
-        r#"{{"jsonrpc": "2.0", "id": 0, "method": "Application.SetVolume", "params": {{ "volume": {vol} }}}}"#
-    );
-    req::<()>(payload.as_bytes(), false)?;
+pub fn set_vol(volume: i8) -> Result<()> {
+    req::<Volume, ()>("Application.SetVolume", &Volume { volume }, false)?;
     Ok(())
 }
 pub fn update_vol(increment: i8) -> Result<()> {
-    let cur_vol = req::<Vol>(br#"{"jsonrpc": "2.0", "id": 0, "method": "Application.GetProperties", "params": { "properties": ["volume"]}}"#, true)?.context("No response")?;
+    let cur_vol = req::<Properties, Vol>(
+        "Application.GetProperties",
+        &Properties {
+            properties: &["volume"],
+        },
+        true,
+    )?
+    .context("No response")?;
     let new_vol = match cur_vol.volume + increment {
         i if i < 0 => 0,
         i if i > 100 => 100,
